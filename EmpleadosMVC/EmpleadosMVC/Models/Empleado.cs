dba286d54e2141a973d55b4bcc31276da7538261
Elkin Oved Ortiz Salazar <eortizs@ucentral.edu.co>
2020-09-06 22:57:36 -0500

    Actualización Categoria

--- EmpleadosMVC/EmpleadosMVC/Models/Empleado.cs
+++ EmpleadosMVC/EmpleadosMVC/Models/Empleado.cs
@@ -12,7 +12,8 @@ namespace EmpleadosMVC.Models
         public int ID { get; set; }
         public string Nombre { get; set; }
         public int Antiguedad { get; set; }
-        public int Edad { get; set; }      
+        public int Edad { get; set; }
+        public string Categoria { get; set; }
     }
     public class EmpleadoDBContext : DbContext 
     {
